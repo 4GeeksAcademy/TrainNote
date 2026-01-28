f8a199b31ed7574d143bd85c96a010c83f0e1a27
Sebaperez12 <sebaperez12@gmail.com>
2026-01-28 21:51:46 +0000

    creacion de vista creat todo form

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -11,7 +11,7 @@ import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 import { Signup } from "./pages/Signup";
 import { Login } from "./pages/Login";
-
+import { CreateTodoForm } from "./pages/CreateTodoForm";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -30,6 +30,7 @@ export const router = createBrowserRouter(
         <Route path="/demo" element={<Demo />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/crear-tarea" element={<CreateTodoForm />} />
       </Route>
     )
 );
