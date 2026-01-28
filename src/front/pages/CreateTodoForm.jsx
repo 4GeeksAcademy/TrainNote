fbd9112d356f16bc2417edc8d83e666048782b52
Sebaperez12 <sebaperez12@gmail.com>
2026-01-28 21:54:08 +0000

    correcion

---
+++ src/front/pages/CreateTodoForm.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const CreateTodoForm = () => {
+
+  return (
+
+    <div className="container text-center"> 
+      <div className="row">
+        <div className="col-3" style={{ backgroundColor: "#FBFBFB" }}>
+         
+        </div>
+        <div className="col-9  " style={{ backgroundColor: "#49BBBD" }}>
+           Generador de tareas
+          <br />
+          Curso: Desarrollo Web
+        </div>
+      </div>
+    
+    </div>
+
+  )
+
+}
