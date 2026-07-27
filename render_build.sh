c782624ea1e3d3665d124e1f437e508b780b4db2
Luis Timaure <luistimaure1204@gmail.com>
2026-07-27 18:28:57 -0400

    Update render_build.sh for pipenv and Flask setup
    Added pipenv installation and environment setup for Flask.

--- render_build.sh
+++ render_build.sh
@@ -5,6 +5,15 @@ set -o errexit
 npm install
 npm run build
 
+pip install pipenv
+
 pipenv install
 
-pipenv run upgrade
+export PYTHONPATH=src
+export FLASK_APP=src/app.py
+
+# 1. Intentar correr migraciones estándar de Flask-Migrate
+pipenv run upgrade || true
+
+# 2. Respaldar con create_all() para garantizar que las tablas existan en PostgreSQL
+python -c "from app import db, app; app.app_context().push(); db.create_all()"
